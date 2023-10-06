08fe7e166dd963fc0eec9295e9f1ebf0ee9b076b
Sergio Franco Sánchez <144788164+sergiofranco17@users.noreply.github.com>
2023-10-06 16:05:58 +0200

    Add files via upload

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <section className="Main">
+            <img src="../images/photo-grid.png" className="Main--photo" />
+            <h1 className="Main--header">Online Experiences</h1>
+            <p className="Main--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,55 @@
+export default [
+    {
+        id: 1,
+        title: "Japanese House in Minecraft",
+        description: "Build some Houses with us in Minecraft.",
+        price: 5,
+        img: "japanese-house.jpg",
+        stats: {
+            rating: 4.7,
+            reviewCount: 33
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Read some Manga with us",
+        description: "Read some of the best Manga with us and join us in this incredible experience.",
+        price: 3,
+        img: "escanor-vs-demon-king.jpg",
+        stats: {
+            rating: 4.85,
+            reviewCount: 245
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Learn to edit photos with experts",
+        description: "Edit your photos with cutting edge tools and make your photos look professional.",
+        price: 14,
+        img: "photoshop.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 2
+        },
+        location: "Online",
+        openSpots: 3,
+    },
+
+    {
+        id: 4,
+        title: "Victoria's Secret tutorial",
+        description: "Learn to pose like a model using these advice from experts.",
+        price: 16,
+        img: "model.jpg",
+        stats:{
+            rating: 5.0,
+            reviewCount: 26
+        },
+        location: "Online",
+        openSpots: 22,
+    }
+]
